741bf5b5000b3101a269bb3ab0628c688243b56b
2015 iMac <2015imac@2015s-iMac.local>
2020-10-15 19:28:45 +0300

    Updates

---
+++ precache-manifest.9515f2f2b72e3744e873f55482872f9e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5e4cf90197c1403ed4fe8ec19a3e7e7",
+    "url": "/smart-kassa/index.html"
+  },
+  {
+    "revision": "b6bd831be3c4cd832623",
+    "url": "/smart-kassa/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a3b264d706689c49593f",
+    "url": "/smart-kassa/static/js/2.59ebc542.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/smart-kassa/static/js/2.59ebc542.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6bd831be3c4cd832623",
+    "url": "/smart-kassa/static/js/main.c2e355ff.chunk.js"
+  },
+  {
+    "revision": "cc29cb285cf70f466c31",
+    "url": "/smart-kassa/static/js/runtime-main.58a23ee0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/smart-kassa/static/media/logo.5d5d9eef.svg"
+  }
+]);
